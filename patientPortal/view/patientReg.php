1edd8fe39c388f38dd8c173d3830e8731294a1aa
mahmud-ashiq <m.ashiqfida@gmail.com>
2024-02-12 19:12:25 +0600

    patient reg form creation

---
+++ patientPortal/view/patientReg.php
@@ -0,0 +1,91 @@
+<html>
+    <body>
+        <h1>Patient Registration</h1>
+        <form method="POST" action="">
+            <table>
+                <tr>
+                    <td>Full Name:</td>
+                    <td><input type="text" name="name"></td>
+                </tr>
+                <tr><td><br> </td></tr>
+                <tr>
+                    <td>Date of Birth: </td>
+                    <td colspan="2"><input type="date" name="dob"></td>
+                    <td></td>
+                </tr>
+                <tr><td><br> </td></tr>
+                <tr>
+                    <td>Sex:</td>
+                    <td><select>
+                        <option name="male">Male</option>
+                        <option name="female">Female</option>
+                    </select></td>
+                </tr>
+                <tr><td><br> </td></tr>
+                <tr>
+                    <td>Contact Number:</td>
+                    <td><input type="text" name="contact"></td>
+                </tr>
+                <tr><td><br> </td></tr>
+                <tr>
+                    <td>Email:</td>
+                    <td><input type="text" name="email"></td>
+                </tr>
+                <tr><td><br> </td></tr>
+                <tr>
+                    <td>Address:</td>
+                    <td><input type="text" name="s_address"></td>
+                </tr>
+                <tr><td></td><td>Street Address</td></tr>
+                <tr>
+                    <td></td>
+                    <td><input type="text" name="city"></td>
+                </tr>
+                <tr><td></td><td>City</td></tr>
+                <tr>
+                    <td></td>
+                    <td><input type="text" name="postal"></td>
+                </tr>
+                <tr><td></td><td>Postal Code</td></tr>
+                <tr><td><br> </td></tr>
+                <tr>
+                    <td>Marital Status:</td>
+                    <td><select>
+                        <option name="married">Married</option>
+                        <option name="single">Single</option>
+                    </select></td>
+                </tr>
+                <tr><td><br> </td></tr>
+                <tr>
+                    <td>Weight (kgs):</td>
+                    <td><input type="text" name="weight"></td>
+                </tr>
+                <tr><td><br> </td></tr>
+                <tr>
+                    <td>Height (inches):</td>
+                    <td><input type="text" name="height"></td>
+                </tr>
+                <tr><td><br> </td></tr>
+                <tr><td><br> </td></tr>
+                <tr><td><h3>In case of emergency</h3></td></tr>
+                <tr>
+                    <td>Emergency contact name:</td>
+                    <td><input type="text" name="emergency_contact"</td>
+                </tr>
+                <tr><td><br> </td></tr>
+                <tr>
+                    <td>Relationship:</td>
+                    <td><input type="text" name="relation"</td>
+                </tr>
+                <tr><td><br> </td></tr>
+                <tr>
+                    <td>Contact Number:</td>
+                    <td><input type="text" name="e_contact"></td>
+                </tr>
+                <tr><td><br> </td></tr>
+                
+                <tr><td><input type="submit" value="Submit" name="submit"</td></tr>
+            </table>
+        </form>
+    </body>
+</html>
