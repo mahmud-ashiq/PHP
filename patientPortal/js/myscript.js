ae9517ebf6a496f29970859c5b569d2fedcf7da6
mahmud-ashiq <m.ashiqfida@gmail.com>
2024-04-22 09:03:27 +0600

    JS validation and CSS

--- patientPortal/js/myscript.js
+++ patientPortal/js/myscript.js
@@ -1,10 +1,32 @@
-<script>
+   
     function validateForm() {
-        var name = document.getElementById("name").value;
-        if(name == "")
+        var sex = document.getElementById("sex").value;
+        if(!sex)
         {
-            alert("Name must be filled (JS validation)");
+            alert("Gender must be selected");
             return false;
         }
+        var address = document.getElementById("address").value;
+        if(address == "")
+        {
+            //alert("asdf");
+            document.getElementById("add").innerHTML = "Address must be filled";
+            return false;
+        }
+        var age = document.getElementById("age").value;
+        age = parseInt(age);
+        if(age < 18 || age > 100 )
+        {
+            alert("Age must be between 18 to 100");
+            return false;
+        }
+        var postal = document.getElementById("postal").value;
+        if(isNaN(postal) || postal.length != 5)
+        {
+            alert("postal code must be 5 digit number (JS validation)");
+            return false;
+        }
+        
+        
+
     }
- </script>
--- patientPortal/view/patientReg.php
+++ patientPortal/view/patientReg.php
@@ -1,7 +1,7 @@
 <?php include '../controller/patientControl.php'; 
-    include '../js/myscript.js';?>
+    include '../css/mystyle.css'; ?>
 <html>
-
+<head><script src="../js/myscript.js"></script></head>
     <body>
         <form method="POST" action="" onsubmit="return validateForm()">
         <fieldset>
@@ -24,9 +24,10 @@
                 <tr><td><br> </td></tr>
                 <tr>
                     <td>Sex:</td>
-                    <td><select>
-                        <option name="male">Male</option>
-                        <option name="female">Female</option>
+                    <td><select id="sex">
+                    <option disabled selected value> -- select an option -- </option>
+                        <option  name="male">Male</option>
+                        <option  name="female">Female</option>
                     </select></td>
                 </tr>
                 <tr><td><br> </td></tr>
@@ -52,12 +53,12 @@
                 <tr><td></td><td>Street Address</td></tr>
                 <tr>
                     <td></td>
-                    <td><input type="text" name="city"></td>
+                    <td><input type="text" name="city" id="address"></td><td><p id="add"></p></td>
                 </tr>
                 <tr><td></td><td>City</td></tr>
                 <tr>
                     <td></td>
-                    <td><input type="text" name="postal"></td>
+                    <td><input type="text" name="postal" id="postal"></td>
                 </tr>
                 <tr><td></td><td>Postal Code</td></tr>
                 <tr><td><br> </td></tr>
@@ -70,8 +71,8 @@
                 </tr>
                 <tr><td><br> </td></tr>
                 <tr>
-                    <td>Weight (kgs):</td>
-                    <td><input type="text" name="weight"></td>
+                    <td>Aged (yrs):</td>
+                    <td><input type="text" name="age" id="age"></td>
                 </tr>
                 <tr><td><br> </td></tr>
                 <tr>
