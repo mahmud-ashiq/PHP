2181f9ec3f52929fbf1b95a47b301ad5fad53560
mahmud-ashiq <m.ashiqfida@gmail.com>
2024-04-29 21:02:43 +0600

    user search php controller

--- patientPortal/controller/searchUser.php
+++ patientPortal/controller/searchUser.php
@@ -1,12 +1,14 @@
 <?php
 include '../models/mydb.php';
 $count=1;
+$data = array();
 $mydb = new Model();
 $conObj = $mydb->OpenCon();
 $result = $mydb->ShowUser($conObj);
 if($result->num_rows>0)
     {
         while($row = $result->fetch_assoc()){
+            $data[] = $row;
             echo "<h3>User ".$count.": </h3>";
           echo "<table><tr><td>Name:</td><td>".$row["name"]."</td></tr>";
           echo "<tr><td>Date of Birth:</td><td>".$row["dob"]."</td></tr>";
@@ -14,6 +16,11 @@ if($result->num_rows>0)
           echo "<tr><td>Email:</td><td>".$row["email"]."</td></tr></table>"; 
           echo "<br>";
           $count++;
+          $json = json_encode($data);
+          //echo $json;
         }
+
     }
 ?>
+<script type="text/javascript"> var jsonData = "<?=$json?>"</script>
+<script type="text/javascript" src="../js/my.js"></script>
